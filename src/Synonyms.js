815c391889466eaf6a2035357178a20c09c673d3
Breanda Barnett <breanda.barnett@gmail.com>
2023-01-23 15:05:42 +0900

    Added phonetics

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,13 +3,11 @@ import React from "react";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <div className="Synonyms">
-        <ul>
-          {props.synonyms.map(function (synonym, index) {
-            return <li key={index}>{synonym}</li>;
-          })}
-        </ul>
-      </div>
+      <ul>
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
+        })}
+      </ul>
     );
   } else {
     return null;
