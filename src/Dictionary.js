7f793dc1a38e9311fe73c24bd4b848cdb9ea56e6
Breanda Barnett <breanda.barnett@gmail.com>
2023-03-04 16:41:38 +0900

    Updated Authorization header for Pexels api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -26,7 +26,7 @@ export default function Dictionary(props) {
     let pexelsApiKey =
       "lKPqM6DvjJM6mSxLIMa1hq7YTUUlILnOpnCxNCro8Brl5BYrNQiirCVc";
     let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
-    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    let headers = { Authorization: `${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
